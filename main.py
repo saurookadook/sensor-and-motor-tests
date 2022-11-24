fb67fdd9ea8f9a4d2ae5dda15127d8a1284e46eb
Andy Maskiell <maskiella@gmail.com>
2022-11-24 11:31:48 -0500

    The program works better than the actual robot lol

--- main.py
+++ main.py
@@ -2,33 +2,43 @@
 
 import sys
 from inspect import getmembers
-from time import sleep
+from time import sleep, time
 
 # `hubs` module: https://pybricks.com/ev3-micropython/hubs.html
 from sensor_and_motor_tests import EV3App
 
 
 def main():
-    test_app = EV3App(
-        name="test app",
-    )
+    test_app = EV3App(name="test app", disable_sound=True)
 
     test_app.boot_up_greeting()
 
+    start_time = time()
+    # main_timer = time()
+    latest_direction = "forwards"
     while True:
-        if test_app._front_touch_sensor.is_pressed:
-            print("Front touch sensor pressed!", file=sys.stderr)
-            test_app._sound.speak("boop bop beep")
-            test_app.drive(speed=75, direction="backwards")
+        test_app.drive(speed=50, direction=latest_direction, duration=-1)
 
-        if test_app._back_touch_sensor.is_pressed:
+        if test_app._front_touch_sensor.is_pressed:
+            test_app.stop()
+            latest_direction = "backwards"
+            print("Front touch sensor pressed!", file=sys.stderr)
+            test_app.say("Ouch, my face!")
+            test_app.drive(speed=75, direction=latest_direction, duration=-1)
+            test_app.say("boop bop beep")
+
+        elif test_app._back_touch_sensor.is_pressed:
+            test_app.stop()
+            latest_direction = "forwards"
             print("Back touch sensor pressed!", file=sys.stderr)
-            test_app._sound.speak("beep bop boop")
-            test_app.drive(speed=75, direction="forwards")
+            test_app.say("Ouch, my butthole!")
+            test_app.drive(speed=75, direction=latest_direction, duration=-1)
+            test_app.say("beep bop boop")
 
         # sleep(0.01)
 
-        if test_app._buttons.buttons_pressed:
+        if test_app._buttons.buttons_pressed or time() - start_time >= 10000:
+            test_app.stop()
             break
 
     test_app.shut_down()
--- sensor_and_motor_tests/app.py
+++ sensor_and_motor_tests/app.py
@@ -4,7 +4,7 @@ from time import sleep
 
 from ev3dev2.button import Button
 from ev3dev2.console import Console
-from ev3dev2.motor import DcMotor
+from ev3dev2.motor import DcMotor, MediumMotor
 from ev3dev2.port import LegoPort
 from ev3dev2.sensor import INPUT_1, INPUT_2
 from ev3dev2.sensor.lego import TouchSensor
@@ -20,7 +20,7 @@ class App:
     _buttons = None
     _sound = None
 
-    def __init__(self, name="", brick_device=None):
+    def __init__(self, name="", brick_device=None, **kwargs):
         self.name = name
 
         self._brick_device = brick_device
@@ -75,38 +75,52 @@ class EV3App(App):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # TODO: better name? also, could this be neater?
+        self.is_silenced = True if kwargs["disable_sound"] else False
+
         self._buttons = Button()
         self._console = Console()
         self._sound = Sound()
 
-        super()._configure_ports_with_mode(
-            self, port_a=LegoPort("outA"), port_b=LegoPort("outB")
-        )
+        port_a = kwargs["port_a"] or LegoPort("outA")
+        port_b = kwargs["port_b"] or LegoPort("outB")
+        self._port_c = LegoPort("outC")
+
+        super()._configure_ports_with_mode(self, port_a=port_a, port_b=port_b)
+
+        self._aux_motor = MediumMotor(self._port_c.address)
+
+        self._aux_motor.on_for_rotations(50, 3)
 
         self._right_motor = DcMotor(self._port_a.address)
         self._left_motor = DcMotor(self._port_b.address)
+        self.current_direction = "forwards"
 
         self._front_touch_sensor = TouchSensor(INPUT_1)
         self._back_touch_sensor = TouchSensor(INPUT_2)
+
+    def boot_up_greeting(self):
+        self.say("Bootin up, baby")
+        self._console.text_at("Bootin up . . .", alignment="C")
+        sleep(1)
+        self._console.text_at("BINGO BANGO!", alignment="C", reset_console=True)
+        self.say("BINGO BANGO!")
+
+    def shut_down(self):
+        self._console.text_at("I am a pickle!", reset_console=True, alignment="C")
+        self.say("I am a pickle!")
+
+        self._console.text_at("Bye forever", alignment="C")
+        self.say("Bye forever")
+
+    def say(self, text, **kwargs):
+        if not self.is_silenced:
+            self._sound.speak(text, **kwargs)
 
     def run(
         self,
     ):
         pass
-
-    def boot_up_greeting(self):
-        self._sound.speak("Bootin up, baby")
-        self._console.text_at("Bootin up . . .", alignment="C")
-        sleep(1)
-        self._console.text_at("BINGO BANGO!", alignment="C", reset_console=True)
-        self._sound.speak("BINGO BANGO!")
-
-    def shut_down(self):
-        self._console.text_at("I am a pickle!", reset_console=True, alignment="C")
-        self._sound.speak("I am a pickle!")
-
-        self._console.text_at("Bye forever", alignment="C")
-        self._sound.speak("Bye forever")
 
     def drive(
         self,
@@ -116,6 +130,9 @@ class EV3App(App):
         direction="forwards",
         duration=1,
     ):
+        if direction != self.current_direction:
+            self.current_direction = direction
+
         if speed and not right_wheel_speed:
             right_wheel_speed = speed if direction == "forwards" else -speed
         if speed and not left_wheel_speed:
@@ -126,5 +143,8 @@ class EV3App(App):
 
         if duration > 0:
             sleep(duration)
-            self._right_motor.stop()
-            self._left_motor.stop()
+            self.stop()
+
+    def stop(self):
+        self._right_motor.stop()
+        self._left_motor.stop()
